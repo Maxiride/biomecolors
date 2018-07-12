4f1344e077e52e9394ef0e3cb1dd7db002bfa5c7
Maxiride <maxiride@gmail.com>
2018-07-12 20:39:08 +0200

    Changed code organization, biomes now organized as struct. I'm rather unsure if I actually need two maps, one by name and one by climate type 🤔

--- main/main.go
+++ main/main.go
@@ -12,94 +12,64 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/lucasb-eyer/go-colorful" // Colors managment
+	"github.com/lucasb-eyer/go-colorful" // Colors management
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/basicfont"
 	"golang.org/x/image/math/fixed"
 	"gopkg.in/ini.v1" // INI parser
 )
 
+// biomeList will contain a map of type biome of all the biomes found by getBiomes indexed with the biome name as the key
+var biomeList = make(map[string][]biome)
+
+// same as above but this map has keys set as climate type
+var biomeListByClimate = make(map[string][]biome)
+
 /*
-Global variables declaration
+The biome struct will contain biomes infos.
+Vanilla reference for the climateZone:
+Biomes are separated into 5 categories.
+The snow-covered biomes are marked in blue with temperature <0.2
+cold in green  with temperature <0.5,
+medium/lush in orange  with temperature <0.95
+and dry/warm in red  with temperature <=2.
 */
 
-// biomeList will contain a slice of all the biomes found by getBiomes
-var biomeList []string
-
-/*
-temperatureGroups will contain biomes grouped by climate zone, I will use shades of those colors for the palette.
-Vanilla reference:
-Biomes are separated into 5 categories. The snow-covered biomes are marked in blue, cold in green,
-medium/lush in orange and dry/warm in red.
-*/
-var temperatureGroups = map[string][]string{
-	"Snowy":  {""},
-	"Cold":   {""},
-	"Medium": {""},
-	"Dry":    {""},
+type biome struct {
+	name        string
+	temperature float64
+	climateZone string
 }
 
+// Used late in prepPalette to check if a color has already been used
 var usedColors [][]float64
 
 func main() {
-	// Prints the number of biomes found, if true it will print also a list of their names
-	totalBiomes := getBiomes(false)
+
+	totalBiomes := getBiomes()
 	fmt.Println("Biomes found: ", totalBiomes)
-
-	// Assign each biome to a temperatureGroups key
-	assignBiomes()
-
-	// Just for debug to ensure that the numbers add up
-	for key := range temperatureGroups {
-		fmt.Println(key, len(temperatureGroups[key]))
-	}
-	fmt.Println(temperatureGroups["Snowy"][1])
 
 	prepPalette()
 }
 
 // getBiomes will parse the WorldBiomes folder, any biome found will be appended to the biomeList. Returns the total number of biomes found
-func getBiomes(test bool) int {
+func getBiomes() int {
 	files, err := ioutil.ReadDir("./WorldBiomes")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, f := range files {
-		// Add the biomes found to the BiomeList slice
-		biomeList = append(biomeList, f.Name())
-	}
-	// If test == true, prints out a list of the biomes found.
-	if test {
-		fmt.Println(biomeList)
+		biomeCategorization(f.Name())
 	}
 
 	return len(files)
 }
 
-// assignBiomes will parse through the biomes found in biomeList, read their temperature via biomeTemp and assign them to a temperatureGroups map key
-func assignBiomes() {
-	for _, biome := range biomeList {
-		temp := biomeTemp(biome)
+// biomeCategorization will parse every biome and create its struct with proper fields
+func biomeCategorization(s string) {
 
-		switch {
-		case temp < 0.2:
-			temperatureGroups["Snowy"] = append(temperatureGroups["Snowy"], biome)
-		case temp < 0.5:
-			temperatureGroups["Cold"] = append(temperatureGroups["Cold"], biome)
-		case temp < 0.95:
-			temperatureGroups["Medium"] = append(temperatureGroups["Medium"], biome)
-		case temp <= 2:
-			temperatureGroups["Dry"] = append(temperatureGroups["Dry"], biome)
-
-		}
-	}
-
-}
-
-// biomeTemp will open each biome config file to retrieve and return its temperature value
-func biomeTemp(s string) float64 {
-
+	// get a biome file
 	input, err := ini.LoadSources(ini.LoadOptions{
 		SkipUnrecognizableLines: true,
 	}, "./WorldBiomes/"+s)
@@ -108,44 +78,73 @@ func biomeTemp(s string) float64 {
 		os.Exit(1)
 	}
 
-	tempS := input.Section("").Key("BiomeTemperature").String()
-	temp, _ := strconv.ParseFloat(tempS, 64)
+	// Get its temperature value from the key BiomeTemperature
+	temp, _ := strconv.ParseFloat(input.Section("").Key("BiomeTemperature").String(), 64)
 
-	return temp
+	// Let's see which climate this biome has
+	climate := ""
+	switch {
+	case temp < 0.2:
+		climate = "Snowy"
+	case temp < 0.5:
+		climate = "Cold"
+	case temp < 0.95:
+		climate = "Medium"
+	case temp <= 2:
+		climate = "Dry"
+
+	}
+
+	// Save the found biomes in their maps
+	biomeList[s] = append(biomeList[s], biome{
+		name:        s,
+		temperature: temp,
+		climateZone: climate,
+	})
+
+	biomeListByClimate[climate] = append(biomeListByClimate[climate], biome{
+		name:        s,
+		temperature: temp,
+		climateZone: climate,
+	})
+
 }
 
-// prepPalette will generate a colour palette for each group in temperatureGroups by calling the respective color settings in
-// isSnowy, isCold etc
+/*
+prepPalette will generate a colour palette for each climate in biomeListByClimate ensuring that all the generated colors falls under the right range with the isSnowy, isCold etc functions.
+
+*/
 func prepPalette() {
 
 	usedColors = [][]float64{}
-	snowyColors := len(temperatureGroups["Snowy"])
+	snowyColors := len(biomeListByClimate["Snowy"])
 	snowyPalette, err := colorful.SoftPaletteEx(snowyColors, colorful.SoftPaletteSettings{isSnowy, 50, true})
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	usedColors = [][]float64{}
-	coldColors := len(temperatureGroups["Cold"])
+	coldColors := len(biomeListByClimate["Cold"])
 	coldPalette, err := colorful.SoftPaletteEx(coldColors, colorful.SoftPaletteSettings{isCold, 50, true})
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	usedColors = [][]float64{}
-	mediumColors := len(temperatureGroups["Medium"])
+	mediumColors := len(biomeListByClimate["Medium"])
 	mediumPalette, err := colorful.SoftPaletteEx(mediumColors, colorful.SoftPaletteSettings{isMedium, 50, true})
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	usedColors = [][]float64{}
-	dryColors := len(temperatureGroups["Dry"])
+	dryColors := len(biomeListByClimate["Dry"])
 	dryPalette, err := colorful.SoftPaletteEx(dryColors, colorful.SoftPaletteSettings{isDry, 50, true})
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Let's organize the palettes
 	palettes := map[string][]colorful.Color{
 		"Snowy":  snowyPalette,
 		"Cold":   coldPalette,
@@ -153,8 +152,9 @@ func prepPalette() {
 		"Dry":    dryPalette,
 	}
 
+	// Draw them!
 	for i := range palettes {
-		printPalette(palettes[i], len(temperatureGroups[i]), i)
+		drawPalette(palettes[i], len(biomeListByClimate[i]), i)
 	}
 
 }
@@ -165,17 +165,18 @@ func isUnique(h, c, L float64) bool {
 	for _, color := range usedColors {
 		if color[0] == h && color[1] == c && color[2] == L {
 			//fmt.Println("This color already exist!")
-			//fmt.Println(usuedColors)
+			//fmt.Println(usedColors)
 			return false
 		}
 	}
 	return true
 }
 
+// All the color checking functions, I guess all of them could be grouped as a interface? And then prepPalette just call check.isSnowy, check.isCold mmm
 func isSnowy(l, a, b float64) bool {
 	h, c, L := colorful.LabToHcl(l, a, b)
 
-	// ok is a placeholder for Go visibility and scope rules
+	// ok is a placeholder for Go visibility and scope rules, do I really need this?? Must be a smarter way
 	ok := false
 
 	// if the the generated color falls into the criteria, check if it has already been generated
@@ -240,13 +241,13 @@ func isDry(l, a, b float64) bool {
 	return ok
 }
 
-// printPalette will take in a palette (created in prepPalette), how many colors it contains and its category name (Snowy, Cold etc from temperatureGroups)
-func printPalette(currentPalette []colorful.Color, colors int, group string) {
+// drawPalette will take in a palette (created in prepPalette), how many colors it contains and its climate name (Snowy, Cold etc)
+func drawPalette(currentPalette []colorful.Color, colors int, group string) {
 	/*
 		The image layout is meant to be on two columns "colored square" "HEX Value" "Biome Name"
 	*/
 
-	// Palette squares side lenght and spacing between each of them
+	// Palette squares side length and spacing between each of them
 	blockw := 40
 	space := 5
 
@@ -256,13 +257,13 @@ func printPalette(currentPalette []colorful.Color, colors int, group string) {
 	// White background
 	draw.Draw(img, image.Rect(0, 0, 800, colors/2*(blockw+space)), image.NewUniform(color.RGBA{255, 255, 255, 255}), image.ZP, draw.Src)
 
-	// Draw on the image
+	// Draw on the image each color of the current palette
 	i := 0
 	for row := 0; row < colors/2; row++ {
 		for j := 0; j <= 1; j++ {
 			draw.Draw(img, image.Rect(400*j, row*(blockw+space), 400*j+2*blockw, row*(blockw+space)+blockw), &image.Uniform{currentPalette[i]}, image.ZP, draw.Src)
 			addLabel(img, 12+400*j, 25+(row*(blockw+space)), strings.ToUpper(currentPalette[i].Hex()))
-			addLabel(img, 120+400*j, 25+(row*(blockw+space)), temperatureGroups[group][i])
+			addLabel(img, 120+400*j, 25+(row*(blockw+space)), biomeListByClimate[group][i].name)
 			i++
 		}
 
@@ -280,6 +281,7 @@ func printPalette(currentPalette []colorful.Color, colors int, group string) {
 	png.Encode(toimg, img)
 }
 
+// Write down on the image the HEX color and the biome name
 func addLabel(img *image.RGBA, x, y int, label string) {
 	col := color.RGBA{127, 255, 0, 255}
 	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
